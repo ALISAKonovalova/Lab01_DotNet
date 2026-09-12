2e4407939aba6fc97cb77bf333d8963618e4ca0c
ALISAKonovalova <alisakonovalova8814@gmail.com>
2026-09-12 11:36:13 +0300

    Step 9: complete student questionnaire program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет от ИСП 242!)))");
-Console.WriteLine("Автор: Коновалова Алиса и Трафимова Машуля");
-Console.WriteLine("Погода класс))");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Привет от ИСП 242!)))");
+// Console.WriteLine("Автор: Коновалова Алиса и Трафимова Машуля");
+// Console.WriteLine("Погода класс))");
+int age = 5;
+double gpa = 12.8;
+bool isStudent = true;
+string name ="Афродита";
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(gpa);
+Console.WriteLine(isStudent);
+var city = "Волжский";
+var year =2026;
+var pi =3.14159;
+var isActive = true;
+string maName ="Куки-наки";
+int myAge= 1998;
+string myGroup ="ИСП 242";
+Console.WriteLine($"МЕНЯ  зовут {myName}, мне {myAge} лет, я учусь в группе {myGroup}.");
+Console.WriteLine("Введите ваше имя:")
+string name2 = Console.ReadLine();
+Console.WriteLine($"Привет, {name2}!");
+Console.WriteLine("Введите ваш возраст:")
+string input = Console.ReadLine();
+int age2 = int.Parse(input);
+Console.WriteLine($"через 10 лет вам будет  {age2 + 10} лет!");
+int x = 10;
+int y = 3;
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.WriteLine(x / y);
+Console.WriteLine(x % y);
+string firstName ="Куки";
+string LastName ="Наки";
+string group ="ИСП-242";
+int birthYear = 2008;
+double gpa =4.5;
+bool hasscholarship = true;
+int currentYear = 2026;
+int age = currentYear - birthYear;
+Console.WriteLine("Студенческое удостоверение");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+
