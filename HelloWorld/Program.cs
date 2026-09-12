322aa94bcd91ad4957a43e8761c2f88fb4c34918
ALISAKonovalova <alisakonovalova8814@gmail.com>
2026-09-12 10:55:10 +0300

    Step 6: first C# program running

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет от ИСП 242!)))");
+Console.WriteLine("Автор: Коновалова Алиса и Трафимова Машуля");
+Console.WriteLine("Погода класс))");
